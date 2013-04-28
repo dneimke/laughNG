ba30df83a1f3a154c0035b215c0b9c2e79f34117
Darren Neimke <darren.neimke@gmail.com>
2013-04-28 13:31:38 +0930

    re-included the video bootstrapping module

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var app = angular.module('videoPlayer', [function() {
-
-}]);
+var app = angular.module('videoPlayer', ['videoModule']);
 
 
 app.config(function($routeProvider) {
@@ -20,25 +18,3 @@ app.config(function($routeProvider) {
             redirectTo: '/'
         });
 });
-
-app.controller('searchController', ['$scope', 'searchModel', function() {
-   return new SearchController($scope, searchModel);
-}]);
- 
-app.controller('videoItemController', ['$scope', 'videoItemModel', function($scope, videoItemModel) {
-  return new VideoItemController($scope, videoItemModel);
-}]);
-
-
-app.factory('videoService', function() {
-  return new VideoService();
-});
- 
-
-app.factory('searchModel', ['videoService', function(videoService) {
-  return new SearchModel(videoService);
-}]);
-
-app.factory('videoItemModel', ['videoService', function(videoService) {
-  return new VideoItemModel(videoService);
-}]);
--- scripts/videos/VideoModule.js
+++ scripts/videos/VideoModule.js
@@ -1,5 +1,31 @@
 'use strict';
 
+// A bootstrapper for the video module
+
+var module = angular.module('videoModule', []);
+
+module.controller('searchController', ['$scope', 'searchModel', function($scope, searchModel) {
+   return new SearchController($scope, searchModel);
+}]);
+ 
+module.controller('videoItemController', ['$scope', 'videoItemModel', function($scope, videoItemModel) {
+  return new VideoItemController($scope, videoItemModel);
+}]);
+
+
+module.factory('videoService', function() {
+  return new VideoService();
+});
+ 
+
+module.factory('searchModel', ['videoService', function(videoService) {
+  return new SearchModel(videoService);
+}]);
+
+module.factory('videoItemModel', ['videoService', function(videoService) {
+  return new VideoItemModel(videoService);
+}]);
+
 
  
 
